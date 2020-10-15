7ebd57f270f8d5d0ab1eb1af8f8457077049d8f2
omirbeck <ommirtull@gmail.com>
2020-10-15 15:26:27 +0600

    refactor: change Quiz state to redux store

--- src/store/reducers/quiz.js
+++ src/store/reducers/quiz.js
@@ -1,8 +1,13 @@
-import { 
-    FETCH_QUIZES_ERROR, 
-    FETCH_QUIZES_START, 
+import {
+    FETCH_QUIZES_ERROR,
+    FETCH_QUIZES_START,
     FETCH_QUIZES_SUCCESS,
-    FETCH_QUIZ_SUCCESS } from "../actions/actionTypes";
+    FETCH_QUIZ_SUCCESS,
+    QUIZ_SET_STATE,
+    FINISH_QUIZ,
+    QUIZ_NEXT_QUESTION,
+    QUIZ_RETRY
+} from "../actions/actionTypes";
 
 const initilaState = {
     quizes: [],
@@ -34,12 +39,42 @@ export default function quizReducer(state = initilaState, action) {
                 loading: false,
                 error: action.error
             }
-            case FETCH_QUIZ_SUCCESS:
-                return {
-                    ...state,
-                    loading: false,
-                    quiz: action.quiz
-                }
+        case FETCH_QUIZ_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                quiz: action.quiz
+            }
+        case QUIZ_SET_STATE: {
+            return {
+                ...state,
+                answerState: action.answerState,
+                results: action.results
+            }
+        }
+        case FINISH_QUIZ: {
+            return {
+                ...state,
+                isFinished: true
+            }
+        }
+        case QUIZ_NEXT_QUESTION: {
+            return {
+                ...state,
+                answerState: null,
+                activeQuestion: action.number
+            }
+        }
+        case QUIZ_RETRY: {
+            return {
+                ...state,
+                results: {},
+                isFinished: false,
+                activeQuestion: 0,
+                answerState: null,
+            }
+        }
+
         default:
             return state;
     }
